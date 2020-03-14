fd89f7df6fdf7617950d2b927e1cb02bee737e6e
Arjun Nakkeeran <arjun.nakkeeran@hm.com>
2020-03-14 19:25:41 +0100

    Pilot version

---
+++ src/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace XSL_Transformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Please specify the xsl filepath and source xml file path. The output xml will be created in the same folder as source xml");
+                Console.WriteLine("XSL Transformer.exe {XSLT File} {XML File}");
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("Please specify a valid xsl file");
+                return;
+            }
+            if (!File.Exists(args[1]))
+            {
+                Console.WriteLine("Please specify a valid source xml file");
+                return;
+            }
+            
+            var xslFile = new FileInfo(args[0]);
+            var sourceXml = new FileInfo(args[1]);
+
+            try
+            {
+                var xslProcessor = new XslProcessor(xslFile, sourceXml);
+                xslProcessor.Transoform();
+                Console.WriteLine("Transformation completed successfully!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error occured while transforming! {e.ToString()}");
+            }
+            
+        }
+    }
+}
---
+++ src/XslProcessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Xml.Xsl;
+
+namespace XSL_Transformer
+{
+    internal class XslProcessor
+    {
+        private readonly FileInfo _xslFile;
+        private readonly FileInfo _soureXml;
+        private FileInfo _outputXml;
+
+        public XslProcessor(FileInfo xslFile, FileInfo sourceXml)
+        {
+            _xslFile = xslFile;
+            _soureXml = sourceXml;
+        }
+
+        public void Transoform()
+        {
+            if (!PrepareOutputXml())
+            {
+                Console.WriteLine("Unable to prepare output xml path. Possibly invalid source xml path.");
+                return;
+            }
+
+            var transformer = new XslCompiledTransform();
+            var sw = new Stopwatch();
+            
+            sw.Start();
+            transformer.Load(_xslFile.FullName);
+            Console.WriteLine("Time to load Xsl {0}", sw.Elapsed);
+            
+            sw.Restart();
+            transformer.Transform(_soureXml.FullName, _outputXml.FullName);
+            Console.WriteLine("Time to transform source xml {0}", sw.Elapsed);
+            
+            Console.WriteLine("Succesfully written output file at {0}", _outputXml.FullName);
+        }
+
+        private bool PrepareOutputXml()
+        {
+            if (_soureXml.Exists)
+            {
+                var outputFilePath = Path.Combine(_soureXml.DirectoryName, $"{Path.GetFileNameWithoutExtension(_soureXml.Name)}_transformed_{DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss")}.{_soureXml.Extension}");
+                _outputXml = new FileInfo(outputFilePath);
+                return true;
+            }
+            return false;
+        }
+    }
+}
